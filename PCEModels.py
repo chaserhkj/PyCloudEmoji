8b0a7d09ee368bacb0f8bcbe03bcfa61a5bb5e15
HuangKangJing <huangkangjing@gmail.com>
2014-11-02 20:12:46 +0800

    Added DataModels

---
+++ PCEModels.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from PyQt5.QtCore import QAbstractTableModel,QAbstractListModel
+from PyQt5.QtCore import QVariant
+from PyQt5.QtCore import Qt
+
+class EmojiModel(QAbstractTableModel):
+    def __init__(self, emojis):
+        self._emojis = [(i["description"], i["emoticon"]) for i in emojis]
+        super(EmojiModel,self).__init__()
+    def rowCount(self, index = None):
+        return len(self._emojis)
+    def columnCount(self, index = None):
+        return 2
+    def data(self, index, role = Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return self._emojis[index.row()][index.column()]
+        else:
+            return QVariant()
+    def setEmojis(self, emojis):
+        self.beginResetModel()
+        self._emojis = [(i["description"], i["emoticon"]) for i in emojis]
+        self.endResetModel()
+
+class CateModel(QAbstractListModel):
+    def __init__(self, cates):
+        self._cates = [i["name"] for i in cates]
+        super(CateModel,self).__init__()
+    def rowCount(self, index = None):
+        return len(self._cates)
+    def data(self, index, role = Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return self._cates[index.row()]
+        else:
+            return QVariant()
+    def setCates(self, cates):
+        self.beginResetModel()
+        self._cates = [i["name"] for i in cates]
+        self.endResetModel()
+
+class RepoModel(QAbstractTableModel):
+    def __init__(self, repos):
+        self._repos = repos
+        super(RepoModel,self).__init__()
+    def rowCount(self, index = None):
+        return len(self._repos)
+    def columnCount(self, index = None):
+        return 2
+    def data(self, index, role = Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return self._repos[index.row()][index.column()]
+        else:
+            return QVariant()
